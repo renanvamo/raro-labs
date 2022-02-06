8e61431b1cfa676507b1394eeb04bfa937e325ff
Renan <renan.vamo@gmail.com>
2022-02-06 20:53:34 -0300

    tests(refactor): move the connection inside a beforeEach block, to execute before the tests

--- tests/pagination.test.js
+++ tests/pagination.test.js
@@ -9,19 +9,22 @@ describe('testing api end to end', () => {
   });
 
   describe('if a valid arguments were given', () => {
-    it('should return an object with "id" and "pagination"', () => {
-      chai.request(app)
+    let connection;
+    beforeEach(() => {
+      connection = chai.request(app)
         .get('/paginacao')
-        .query({ paginaAtual: 1, quantidadePaginas: 5 })
+        .query({ paginaAtual: 1, quantidadePaginas: 5 });
+    });
+  
+    it('should return an object with "id" and "pagination"', () => {
+      connection
         .then((res) => {
           expect(res).to.have.property('id');
           expect(res).to.have.property('paginacao');
         });
     });
     it('should return an status 200"', () => {
-      chai.request(app)
-        .get('/paginacao')
-        .query({ paginaAtual: 1, quantidadePaginas: 5 })
+      connection
         .then((res) => {
           expect(res).to.have.status(200);
         });
