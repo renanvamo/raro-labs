b27a57fef851b9fb3ab69e99893ee7ccbe402f30
Renan <renan.vamo@gmail.com>
2022-02-06 20:41:37 -0300

    tests: create first e2e test

--- tests/pagination.test.js
+++ tests/pagination.test.js
@@ -1,5 +1,20 @@
-describe('testing api', () => {
-  it('should pass', () => {
-    
+const chaiHttp = require('chai-http');
+const chai = require('chai');
+let app = require('../src/api/app');
+const { expect } = chai;
+
+describe('testing api end to end', () => {
+  before(() => {
+    chai.use(chaiHttp);
+  });
+
+  it('if two valid arguments were given, should return an object with "id" and "pagination"', () => {
+    chai.request(app)
+      .get('/paginacao')
+      .query({ paginaAtual: 1, quantidadePaginas: 5 })
+      .then((res) => {
+        expect(res).to.have.property('id');
+        expect(res).to.have.property('paginacao');
+      });
   });
 });
