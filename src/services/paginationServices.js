a2eab96cd132759dfdad5832307d6a5d376fe5ef
Renan <renan.vamo@gmail.com>
2022-02-06 10:44:29 -0300

    refactor: call a function to create a new pagination, and return to controller

--- src/services/paginationServices.js
+++ src/services/paginationServices.js
@@ -1,13 +1,21 @@
-const paginationModels = require('../models/paginationModels')
+const { createPagination } = require('../utils');
+
+const paginationModels = require('../models/paginationModels');
 
 const setPagination = (quantidadePaginas, paginaAtual) => {
+  const qtdPaginasNum = Number(quantidadePaginas);
+  const paginaAtualNum = Number(paginaAtual);
+
   const id = paginationModels.setPagination();
+  let paginacao;
+  
+  paginacao = createPagination(qtdPaginasNum, paginaAtualNum);
+  
+  const result = { id, paginacao };
 
-  const result = { id, paginacao: [] }
-
-  return result
+  return result;
 }
 
 module.exports = {
   setPagination
-}
+};
